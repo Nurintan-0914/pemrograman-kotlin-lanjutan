cf12193fa25b74b153ec3161bacb4f9a9f1b1806
Nurintan <nurintann0914@gmail.com>
2024-09-10 12:10:37 +0700

    Add Set.kt

--- src/main/kotlin/Set.kt
+++ src/main/kotlin/Set.kt
@@ -1,16 +1,14 @@
 fun main(){
-    //set<T> stores unique elements; ther order is generaly undefined.
     val numbers = setOf(1, 2, 3, 4)
-    println("Number of elements: ${numbers.size}")
-    if (numbers.contains(1)) println("1, is in the set")
+    println("Numbers of elements: ${numbers.size}")
+    if (numbers.contains(1)) println("1 is in the set")
 
     val numbersBackwards = setOf(4, 3, 2, 1)
     println("The sets are equal: ${numbers == numbersBackwards}")
 
-    //the default implmentation of set - linkedhashset - preserve the order of elements insertion
     println(numbers.first() == numbersBackwards.first())
     println(numbers.first() == numbersBackwards.last())
 
-    val strings = hashSetOf("a", "b", "c", "c") //hash set value unik
-    println("My set values are"+strings)
+    val strings = hashSetOf("a", "b", "c", "c")
+    println("My set values are" +strings)
 }
