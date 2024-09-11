cb5c87289969f33566987ff1afc24add37a920cc
Nurintan <nurintann0914@gmail.com>
2024-09-11 08:37:59 +0700

    Add ReturnForLoop.kt

---
+++ src/main/kotlin/ReturnForLoop.kt
@@ -0,0 +1,17 @@
+fun main(args: Array<String>){
+    returnLoop()
+}
+
+fun returnLoop(){
+    var x:Int
+    println("example of return in For-Loop")
+    for (x in 1..10){
+        if (x < 5){
+            print("${x - 1} ")
+        } else {
+            return
+        }
+        print("$x ")
+    }
+    print("tidak akan pernah dieksekusi")
+}
